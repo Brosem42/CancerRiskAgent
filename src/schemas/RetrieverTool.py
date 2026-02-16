0c1c8083eff38fc8d903b0d0c7dab5098b3febb3
Brianna <brosem42@gmail.com>
2026-02-16 13:34:14 -0500

    Add update

--- src/schemas/RetrieverTool.py
+++
@@ -1,9 +0,0 @@
-# load dotenv + imports for retriever tool
-import os
-from dotenv import load_dotenv
-from langchain_chroma import Chroma
-from langchain.tools import tool
-from langchain import hub
-
-# Load environment variables from .env file
-load_dotenv()
