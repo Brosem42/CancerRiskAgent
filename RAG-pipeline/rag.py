2486fde5ae8b71b7be50b728744b53e9980ed57e
Brianna <brosem42@gmail.com>
2026-02-18 23:52:41 -0500

    Add verification

--- RAG-pipeline/rag.py
+++ RAG-pipeline/rag.py
@@ -6,6 +6,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, StateGraph, END
 from langgraph.constants import END
 from typing_extensions import List, TypedDict
+from langgraph.graph.message import add_messages
 
 #module imports
 from scripts.doc_retrieval import DocumentBaseRetriever
@@ -22,3 +23,74 @@ system_prompt = (
     "\n\nHere is the patient data:"
     "{context}"
 )
+
+# invoke a retriever and a prompt
+retriever = DocumentBaseRetriever()
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{question}")
+    ]
+)
+
+# define the state of out graph
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+    issues_report: str
+    issues_detected: str
+    messages: Annotated[list, add_messages]
+
+# defining my 4 nodes for retrieval in langgraph for retrieve, generate, double_check, and doc_finalizer
+
+#start with retrieval
+def retrieve(state: State):
+    retrieved_docs = retriever.invoke(state["messsges"][-1].content)
+    print(retrieved_docs)
+    return {"context": retrieved_docs}
+
+#generate function
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke(
+        {"question": state["messages"][-1].content, "content": docs_content}
+    )
+    response = llm.invoke(messages)
+    print(response.content)
+    return {"answer": response.content}
+
+#add validation content check
+#in production I would implement human in the loop techniques as a verification process, 
+# here in this instance I will just use an llm to implement verfication of content issues
+# implment validation with a thinking step
+def double_check(state: State):
+    result = llm.invoke([{
+        "role": "user",
+        "content": (
+            f"Review the following clinical decision support project for any violations of HIPAA compliance rules for quality assurance, PHI/PII breach of security, and clinical delivery standards that align with a high level of patient care."
+            f"Return 'ISSUES FOUND' followed by any issues detected or 'NO ISSUES': {state['answer']}"
+        )
+    }]) 
+    #extract actual response after thinking block
+    content = result.content
+    if "</think>" in content:
+        actual_response = content.split("</think>", 1)[1].strip()
+    else:
+        actual_response = content.strip()
+
+    if "ISSUES FOUND" in actual_response:
+        print("issues detected")
+        return {
+            "issues_report": actual_response.split("ISSUES FOUND", 1)[1].strip(),
+            "issues_detected": True
+        }
+    print("no issues detected")
+    return {
+        "issues_report": "",
+        "issues_detected": False
+    }
+
+
+
