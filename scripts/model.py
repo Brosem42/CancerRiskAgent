488627fce53cb46ca910e11d48ff449a19859340
Brianna <brosem42@gmail.com>
2026-02-20 02:04:20 -0500

    fix import

--- scripts/model.py
+++ scripts/model.py
@@ -1,10 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_classic.embeddings import CacheBackedEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.embeddings import CacheBackedEmbeddings
 
 # invoke my model in google
 llm = ChatGoogleGenerativeAI(
