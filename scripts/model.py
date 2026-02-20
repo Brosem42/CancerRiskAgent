fec02af9f3f4a132499e7fbc3bc1ae2325ac9dc8
Brianna <brosem42@gmail.com>
2026-02-20 18:40:56 -0500

    Update import model

--- scripts/model.py
+++ scripts/model.py
@@ -1,14 +1,16 @@
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
 
 from langchain.embeddings import CacheBackedEmbeddings
+api_key = os.getenv("GOOGLE_API_KEY")
 
 # invoke my model in google
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-pro", 
+    model="gemini-1.5-pro",
+    GOOGLE_API_KEY=api_key,
     temperature=0,
     max_output_tokens=2048,
     streaming=True  
